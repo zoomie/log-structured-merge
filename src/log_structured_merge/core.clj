19aa9a04a1f59e9206a9ce81967b5ffdf0b00174
zoomie <andrew.arderne@protonmail.com>
2020-04-05 13:42:37 +0100

    the memtable now saves to a different file each time

--- src/log_structured_merge/core.clj
+++ src/log_structured_merge/core.clj
@@ -8,12 +8,21 @@
 (defn dict-length [dict]
   (reduce (fn [count _] (inc count)) 0 dict))
 
+(def ^:dynamic *datafile-num* 0)
+
+(defn get-data-file []
+  (def ^:dynamic *datafile-num* (inc *datafile-num*))
+  (let [file (apply str *datafile-num* "data.txt")]
+    (apply str "datadir/" file)))
+
+
 (defn to-file [dict]
-  (doseq [tuple dict]
-    (let [key (nth tuple 0)
-          value (nth tuple 1)]
-      (let [in-txt (apply str key ":" value "\n")]
-        (spit "data.txt" in-txt :append true)))))
+  (let [data-file (get-data-file)]
+    (doseq [tuple dict]
+      (let [key (first tuple)
+            value (second tuple)]
+        (let [in-txt (apply str key ":" value "\n")]
+          (spit data-file in-txt :append true))))))
 
 (defn update-db [key value]
   (if (<= 2 (dict-length memetable))
@@ -22,6 +31,11 @@
       (def memetable {})))
   (def memetable (assoc memetable key value)))
 
+(update-db "a" 1)
+(update-db "b" 2)
+(update-db "c" 3)
+(update-db "d" 4)
+(println memetable)
 
 ; Getting data
 (defn tuple-saver [dict raw]
